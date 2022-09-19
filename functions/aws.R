3b4e341767f25220ba055eef78461d40c3bfbfd1
Nathan Elliott <nelliott13@users.noreply.github.com>
2022-09-19 17:00:43 -0500

    Add skeleton files

---
+++ functions/aws.R
@@ -0,0 +1,13 @@
+# AWS functions
+
+# Functions for interacting with data in Amazon buckets
+
+require(aws.s3)
+
+
+# Open and test connection
+test_aws <- function(buckets) {
+  
+  require()
+  
+}
